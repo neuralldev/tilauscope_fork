30fc8588f90b0013696c5408a282f43f75e726c1
thierry gluzman <thierry.gluzman@gmail.com>
2026-08-08 07:31:40 +0200

    TilauScope 4.2.10
    Published from main@c476c1ee6.

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '4.2.9'
+__version__ = '4.2.10'
 __revision__ = ''
 __build__ = '1'
 __artisan_os__ = 'macOS'
@@ -10,4 +10,4 @@ __release_sponsor_name__ = 'powered by TilauScope'
 __release_sponsor_domain__ = 'tilauscope.org'
 __release_sponsor_url__ = 'https://github.com/neuralldev/tilauscope_fork/releases'
 
-__signature__ = '32cab727fef80e1d9ee2057e10672585f73c291dcf552c8fcffbe8b714e6da1d0b58c4960abda766e11ccb458dcc3eed2ded78dac6ad19c9c063afe3487ff503'
+__signature__ = 'c950343eb95e3e88707b5f3e9f86295c6dc5bf27f8322e3e1630aa3dcc344aeb10ab96497f87fe74b68dc5ad8dbf0e9fd1c4731bfbab328e8119676d45bc000e'
